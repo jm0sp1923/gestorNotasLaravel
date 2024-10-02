9623c4645587f802230218f379ed67c077289903
jm0sp1923 <jm0sp1923@gmail.com>
2024-10-01 21:17:12 -0500

    .env

--- app/Models/Nota.php
+++ app/Models/Nota.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Nota extends Model
 {
-    use HasFactory;
+    use HasFactory; 
     
     // Definir la tabla asociada
     protected $table = 'notas';
